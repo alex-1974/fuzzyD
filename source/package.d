da46b4317cecbb842ba3ca6367bcfc827b9c08ff
Alexander Leisser <alexander@leisser.org>
2015-03-14 10:30:49 +0100

    policy-based-design und mixin template beispiele

---
+++ source/package.d
@@ -0,0 +1,25 @@
+/**
+* Proof of concepts
+*
+* Verschiedene Module, die demonstrieren, wie man in D programmieren kann.
+*
+* Author: Alexander Leisser <alexander@leisser.org>
+* Date: 12.02.2015
+* Version: 1.0alpha
+* History:
+*   V1 is initial version
+*
+* License: free to use
+*
+* Examples:
+* <ol>
+*   <li><a href="policy_based_design-proof_of_concept.html">Policy Based Design Logger</a></li>
+*   <li><a href="mixin_logger-proof_of_concept.html">Mixin Based Logger</a></li>
+* </ol>
+**/
+module proof_of_concept;
+
+import std.stdio;
+
+public import proof_of_concept.mixinlogger;
+public import proof_of_concept.pbdlogger;
